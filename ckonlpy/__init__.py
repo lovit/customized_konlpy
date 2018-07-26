89d5e99e19dce559428b997711dd3427243bbd51
lovit <soy.lovit@gmail.com>
2018-07-27 05:03:12 +0900

    Rename selector -> evaluator

--- ckonlpy/__init__.py
+++ ckonlpy/__init__.py
@@ -4,11 +4,11 @@ __author__ = 'Lovit'
 __license__ = 'GPL v3'
 __copyright__ = 'Copyright 2017 Lovit'
 
-from . import tag
-from . import custom_tag
 from .dictionary import CustomizedDictionary
 from .utils import installpath
 from .utils import load_dictionary
 from .utils import loadtxt
 from .utils import load_wordset
 from .utils import load_replace_wordpair
+from . import custom_tag
+from . import tag
