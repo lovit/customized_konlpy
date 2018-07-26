7617c1c2b2af5eeb51c4edf442bd2a6df26c6522
lovit <soy.lovit@gmail.com>
2018-07-26 22:14:29 +0900

    Select order by high score

--- ckonlpy/custom_tag/_selector.py
+++ ckonlpy/custom_tag/_selector.py
@@ -1,35 +1,55 @@
 class SimpleEvaluator:
     def __init__(self, preference=None):
-        self.weight = {
-            'max_length_of_noun': 0.5,
-            'num_of_nouns': -0.2,
-            'num_of_words': -0.1,
-            'exist_noun'  : 0.2,
-            'single_word' : -0.1
-        }
+        self.weight = (
+            ('max_length_of_noun', 0.5),
+            ('exist_noun', 0.2),
+            ('single_word', -0.1)
+        )
         self.preference = preference if preference else {}
 
-    def select(self, candidates):
-        scores = []
-        for candidate in candidates:
-            score = self.score(candidate)
-            scores.append(score)
-        best = sorted(enumerate(scores), key=lambda x:x[1], reverse=True)[0][0]
-        return candidates[best]
+    def select(self, candidates, debug=False):
 
-    def evaluate(self, wordpos_list):
-        max_length_of_noun = _max_length_of_noun(wordpos_list)
-        num_of_nouns = _num_of_nouns(wordpos_list)
-        num_of_words = _num_of_words(wordpos_list)
-        exist_noun = num_of_nouns > 0
-        single_word = num_of_words == 1
+        def evaluating_format(wordpos_list):
+            formed = (
+                wordpos_list,
+                wordpos_list[0][2],
+                wordpos_list[-1][3],
+                self.evaluate(wordpos_list, debug)
+            )
+            return formed
 
-        return (max_length_of_noun * self.weight.get('max_length_of_noun', 0)
-                + num_of_nouns * self.weight.get('num_of_nouns', 0)
-                + num_of_words * self.weight.get('num_of_words', 0)
-                + exist_noun * self.weight.get('exist_noun', 0)
-                + single_word * self.weight.get('single_word', 0)
-               )
+        def is_overlab(b, e, target):
+            return (b < target[2]) and (target[1] < e)
+
+        scoreds = [evaluating_format(c) for c in candidates]
+
+        # sorting rules. (score, begin index)
+        scoreds = sorted(scoreds, key=lambda x:(-x[3], x[1]))
+
+        selected = []
+        while scoreds:
+            selected.append(scoreds.pop(0))
+            b = selected[-1][1]
+            e = selected[-1][2]
+            scoreds = [c for c in scoreds if not is_overlab(b, e, c)]
+
+        return selected
+
+    def evaluate(self, wordpos_list, debug=False):
+
+        scores = (
+            _max_length_of_noun(wordpos_list),
+            _num_of_nouns(wordpos_list) > 0,
+            _num_of_words(wordpos_list) == 1
+        )
+
+        if debug:
+            print('\n{}'.format(wordpos_list))
+            for score, (field, weight) in zip(scores, self.weight):
+                print('{}, w={}, s={}, prod={}'.format(
+                        field, weight, score, weight * score))
+
+        return sum((score * weight for score, (_, weight) in zip(scores, self.weight)))
 
 def _max_length_of_noun(wordpos_list):
     satisfied = [len(wordpos[0]) for wordpos in wordpos_list if wordpos[1] == 'Noun']
@@ -40,6 +60,3 @@ def _num_of_nouns(wordpos_list):
 
 def _num_of_words(wordpos_list):
     return len(wordpos_list)
-
-def _exist_no_noun(wordpos_list):
-    return _num_of_nouns(wordpos_list) == 0
