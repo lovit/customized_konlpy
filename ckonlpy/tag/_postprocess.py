947bb9401cd33d778d5406e81334d18e7f79f5ad
lovit <soy.lovit@gmail.com>
2018-07-29 23:44:47 +0900

    Add ngram interface

--- ckonlpy/tag/_postprocess.py
+++ ckonlpy/tag/_postprocess.py
@@ -1,11 +1,23 @@
+from collections import defaultdict
+
 class Postprocessor:
-    def __init__(self, base_tagger, stopwords=None, passwords=None, passtags=None, replace=None):
+    def __init__(self, base_tagger, stopwords=None,
+     passwords=None, passtags=None, replace=None, ngrams=None):
         self.base_tagger = base_tagger
         self.stopwords = stopwords if stopwords else None
         self.passwords = passwords if passwords else None
         self.passtags = passtags if passtags else None
         self.replace = replace if replace else None
-        
+        self.ngrams = self._prepare_ngram_converter(ngrams)
+
+    def _prepare_ngram_converter(self, ngrams):
+        if not ngrams:
+            return None
+        first_to_ngram = defaultdict(lambda: [])
+        for ngram in ngrams:
+            first_to_ngram[ngram[0]].append(ngram)
+        return dict(first_to_ngram)
+
     def tag(self, phrase):
         def to_replace(w):
             if w in self.replace:
@@ -16,7 +28,14 @@ class Postprocessor:
                 return w
             return (w_, w[1]) if isinstance(w_, str) else w_
 
+        def as_ngram(words):
+            # TODO
+            return words
+
         words = self.base_tagger.pos(phrase)
+
+        if self.ngrams:
+            words = as_ngram(words)
         if self.stopwords:
             words = [w for w in words if not ((w in self.stopwords) or (w[0] in self.stopwords))]
         if self.passwords:
